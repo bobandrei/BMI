db958b3573bcafb02ceb71d298f8ae655f1d702a
bobandrei <bobandrei26@gmail.com>
2021-03-02 21:13:28 +0200

    create_gaz_app
    homework

---
+++ experimente/Gas.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Gas {
+
+	public static void main(String[] args) 
+	{	
+		Scanner input = new Scanner( System.in );
+		
+		int miles;
+		int gallons;
+		int totalMiles = 0;
+		int totalGallons = 0;
+		double milesPerGallon;
+		double totalMilesPerGallon;
+		
+		System.out.print("Enter miles (-1 to quit): ");
+		miles = input.nextInt();
+		
+		while( miles != -1 )
+		{
+			System.out.print("Enter gallons: ");
+			gallons = input.nextInt();
+			
+			totalMiles += miles;
+			totalGallons += gallons;
+			
+			if( totalGallons != 0 )
+			{
+				totalMilesPerGallon = (double) totalMiles / totalGallons;
+				System.out.printf("Total MPG: %.2f\n" , totalMilesPerGallon);
+			}
+			
+			System.out.print("Enter miles (-1 to quit): ");
+			miles = input.nextInt();
+		}
+	}
+
+}
